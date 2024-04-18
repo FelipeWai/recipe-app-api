0d30eb6952321f7ae248c8259d699bb3c2c6fbc3
Felipe Wai <felipewai.dev@gmail.com>
2024-04-18 18:44:50 -0300

    pyscopg2 installed

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """ Add x and y and return result """
+
+    return x + y
+
+
+def subtract(x, y):
+    """ Subtract x and y and return the result """
+
+    return y - x
