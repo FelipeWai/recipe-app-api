2887b4b58dd6e56ccda74a9813a5534f36aff6be
Felipe Wai <felipewai.dev@gmail.com>
2024-04-24 17:43:03 -0300

    recipe serializer

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+"""
+Views for Recipe APIs
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Recipe
+from recipe import serializers
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs"""
+    serializer_class = serializers.RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer(self):
+        """Return the serializer class for request"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
